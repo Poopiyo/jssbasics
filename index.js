5c17e83737a62f78e1394cec923d74c0052832f6
Peter Opiyo <peter.opiyo@student.moringaschool.com>
2021-11-11 09:28:33 +0300

    Basics

---
+++ index.js
@@ -0,0 +1,21 @@
+// my first Javascript code!
+console.log("Hello world");
+var firstName = 'peter'; // string literal
+console.log(firstName);
+var lastName = 'opiyo';
+console.log(lastName);
+var firstNumber = 3; // Number literal
+console.log(firstNumber);
+let interestRate = 2.3;
+interestRate = 5;
+console.log(interestRate);
+const depreciatingValue = 1.5;
+console.log(depreciatingValue);
+var person = {
+    name: 'peter',
+    Age: 50
+};
+//person.name = 'Petro'; //dot notation
+console.log(person);
+var selectedColors = ['green', 'blue', '2'];
+console.log(selectedColors.length);
